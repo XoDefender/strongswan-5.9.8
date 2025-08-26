39f71ecc8dc3921b804260cd3d9f2717a1f81ee7
xodefender <kir.zhilnikov37@internet.ru>
2025-08-26 13:51:00 +0300

    find cert by id data item

--- src/charon-nm/nm/nm_service.c
+++ src/charon-nm/nm/nm_service.c
@@ -426,9 +426,10 @@ static bool add_auth_cfg_cert(NMStrongswanPluginPrivate *priv,
 		pin = (char*)nm_setting_vpn_get_secret(vpn, "password");
 		if (pin)
 		{
-			char* cert_id;
-			cert_id = (char*)nm_setting_vpn_get_data_item(vpn, "usercert-id");
-			id = find_smartcard_key(priv, pin, cert_id);
+			char* cert_id_secret, *cert_id_data_item;
++			cert_id_secret = (char*)nm_setting_vpn_get_secret(vpn, "usercert-id");
++			cert_id_data_item = (char*)nm_setting_vpn_get_data_item(vpn, "usercert-id");
++			id = find_smartcard_key(priv, pin, cert_id_secret ? cert_id_secret : cert_id_data_item);
 		}
 		if (!id)
 		{
